d98f4dd7d4410a7c87262d931507de74aa0de997
NARodman <ninaalanier8@gmail.com>
2025-08-26 12:05:23 -0400

    Initial Commit: Passowrd strength check solution with tests

---
+++ PasswordCheck.Tests/UnitTest1.cs
@@ -0,0 +1,85 @@
+﻿using Xunit;
+using PasswordCheck;
+
+
+namespace PasswordCheck.Tests
+{
+
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void EmptyPassword_ReturnsInvalid()
+        {
+            var check = new Password();
+            Assert.Equal("INELIGABLE", check.StrengthCheck(""));
+        }
+
+        [Fact]
+
+        public void OnlyLowercase_ReturnsWeak()
+        {
+            var check = new Password();
+            Assert.Equal("WEAK", check.StrengthCheck("abc"));
+        }
+        [Fact]
+        public void OnlyUppercase_ReturnsWeak()
+        {
+            var check = new Password();
+            Assert.Equal("WEAK", check.StrengthCheck("ABC"));
+        }
+        [Fact]
+        public void OnlyDigits_ReturnsWeak()
+        {
+            var check = new Password();
+            Assert.Equal("WEAK", check.StrengthCheck("123"));
+        }
+        [Fact]
+        public void OnlySymbols_ReturnsWeak()
+        {
+            var check = new Password();
+            Assert.Equal("WEAK", check.StrengthCheck("@#$"));
+        }
+        [Fact]
+        public void LowercaseAndUppercase_ReturnsMedium()
+        {
+            var check = new Password();
+            Assert.Equal("MEDIUM", check.StrengthCheck("aBc"));
+        }
+        [Fact]
+        public void LowercaseAndDigits_ReturnsMedium()
+        {
+            var check = new Password();
+            Assert.Equal("MEDIUM", check.StrengthCheck("a1b2c3"));
+        }
+        [Fact]
+        public void LowercaseAndSymbols_ReturnsMedium()
+        {
+            var check = new Password();
+            Assert.Equal("MEDIUM", check.StrengthCheck("a@b#c$"));
+        }
+        [Fact]
+        public void UppercaseAndDigits_ReturnsMedium()
+        {
+            var check = new Password();
+            Assert.Equal("MEDIUM", check.StrengthCheck("A1B2C3"));
+        }
+        [Fact]
+        public void UppercaseAndSymbols_ReturnsMedium()
+        {
+            var check = new Password();
+            Assert.Equal("MEDIUM", check.StrengthCheck("A@B#C$"));
+        }
+        
+        [Fact]
+        public void AllCriteriaMet_ReturnsStrong()
+        {
+            var check = new Password();
+            Assert.Equal("STRONG", check.StrengthCheck("Abc1!"));
+        }
+
+
+
+    }
+}
+
---
+++ PasswordCheck/Password.cs
@@ -0,0 +1,49 @@
+﻿
+
+using System.Security.Cryptography;
+
+namespace PasswordCheck
+{
+
+
+
+    public class Password
+    {
+        public string StrengthCheck(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return "INELIGABLE";
+
+            bool hasUpper = false;
+            bool haslower = false;
+            bool hasDigit = false;
+            bool hasSymbol = false;
+
+            foreach (char ch in password)
+            {
+                if (char.IsUpper(ch)) hasUpper = true;
+                else if (char.IsLower(ch)) haslower = true;
+                else if (char.IsDigit(ch)) hasDigit = true;
+                else hasSymbol = true;
+            }
+
+            int criteriaMet = 0;
+            if (hasUpper) criteriaMet++;
+            if (haslower) criteriaMet++;
+            if (hasDigit) criteriaMet++;
+            if (hasSymbol) criteriaMet++;
+
+            if (criteriaMet == 0)
+                return "INELIGABLE";
+            else if (criteriaMet == 1)
+                return "WEAK";
+            else if (criteriaMet == 2 || criteriaMet == 3)
+                return "MEDIUM";
+            else
+                return "STRONG";
+
+
+        }
+
+    }
+}
